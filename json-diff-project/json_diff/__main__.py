1a59bd22e81ecb0ac91061e923c587cb1e2edbae
Alessandro <alexs2394@gmail.com>
2023-06-10 16:23:31 +0200

    delete config.py

--- json-diff-project/json_diff/__main__.py
+++ json-diff-project/json_diff/__main__.py
@@ -1,5 +1,4 @@
 import sys
-from json_diff.config import load_config
 from json_diff.walk_directory_tree import walk_directory_tree
 from json_diff.utils.write_csv import write_csv
 
@@ -8,7 +7,6 @@ def main():
     args = sys.argv[1:]
     print(args)
     return
-    config = load_config()
     changes = walk_directory_tree(config.path1, config.path2)
     write_csv(changes)
 
